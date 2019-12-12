bc4f38443537aaab5646439e34c33232a03b90a9
ybc <bichengying@gmail.com>
2019-12-11 23:52:09 -0800

    Change the style to rtd

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'blog'
+project = 'YBC'
 copyright = '2019, Bicheng Ying'
 author = 'Bicheng Ying'
 
@@ -28,6 +28,7 @@ author = 'Bicheng Ying'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -45,6 +46,7 @@ language = 'zh_CN'
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# The master toctree document.
 master_doc = 'index'
 
 
@@ -53,7 +55,7 @@ master_doc = 'index'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
